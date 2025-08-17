ae956e39b37adf74ad3010baea34940760fbb40f
fridoa <fridoafriyanto3@gmail.com>
2025-08-17 19:47:59 +0700

    feat(auth): add registration endpoint with validation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import express from 'express';
-import router from './routes/api';
+import express from "express";
+import router from "./routes/api";
+import bodyParser from "body-parser";
 
-const app  = express();
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
 
 const PORT = 3000;
 
-app.use("/api", router);
+app.use("/api/v1", router);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
