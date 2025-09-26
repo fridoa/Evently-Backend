4ed560ba807fff7e033ccd799ea69bb9b6e2bb34
fridoa <fridoafriyanto3@gmail.com>
2025-09-26 17:10:52 +0700

    feat(region): add region management endpoints and model for provinces, regencies, districts, and villages

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,6 +6,7 @@ import mediaMiddleware from "../middlewares/media.middleware";
 import { ROLES } from "../utils/constant";
 import mediaController from "../controllers/media.controller";
 import categoryController from "../controllers/category.controller";
+import regionController from "../controllers/region.controller";
 
 const router = express.Router();
 
@@ -26,6 +27,13 @@ router.get("/category", categoryController.findAll);
 router.get("/category/:id", categoryController.findOne);
 router.put("/category/:id", [authMiddleware, aclMiddleware([ROLES.ADMIN])], categoryController.update);
 router.delete("/category/:id", [authMiddleware, aclMiddleware([ROLES.ADMIN])], categoryController.remove);
+
+router.get("/regions", regionController.getAllProvinces);
+router.get("/regions/:id/province", regionController.getProvince);
+router.get("/regions/:id/regency", regionController.getRegency);
+router.get("/regions/:id/district", regionController.getDistrict);
+router.get("/regions/:id/village", regionController.getVillage);
+router.get("/regions-search", regionController.findByCity);
 
 router.post("/media/upload-single", [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]), mediaMiddleware.single("file")], mediaController.single);
 router.post("/media/upload-multiple", [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]), mediaMiddleware.multiple("files", 5)], mediaController.multiple);
